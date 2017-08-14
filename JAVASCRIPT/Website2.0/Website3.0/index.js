cb2b559cd6badb6412a969048444536c9d16ed64
Crista Falk <Crista2019@users.noreply.github.com>
2017-08-13 21:10:42 -0700

    Create index.js

---
+++ JAVASCRIPT/Website2.0/Website3.0/index.js
@@ -0,0 +1,39 @@
+// Directory Link Animations
+function colorChange1() {
+  document.getElementById("link1").style.WebkitAnimation = "color_change_animation 1s infinite";
+  document.getElementById("link1").style.animation = "color_change_animation 1s infinite";
+}
+function colorChange2() {
+  document.getElementById("link2").style.WebkitAnimation = "color_change_animation 1s infinite";
+  document.getElementById("link2").style.animation = "color_change_animation 1s infinite";
+}
+function colorChange3() {
+  document.getElementById("link3").style.WebkitAnimation = "color_change_animation 1s infinite";
+  document.getElementById("link3").style.animation = "color_change_animation 1s infinite";
+}
+function colorChange4() {
+  document.getElementById("link4").style.WebkitAnimation = "color_change_animation 1s infinite";
+  document.getElementById("link4").style.animation = "color_change_animation 1s infinite";
+}
+function normal1() {
+  document.getElementById("link1").style.WebkitAnimation = "color_change_animation 5s infinite";
+  document.getElementById("link1").style.animation = "color_change_animation 5s infinite";
+}
+function normal2() {
+  document.getElementById("link2").style.WebkitAnimation = "color_change_animation 5s infinite";
+  document.getElementById("link2").style.animation = "color_change_animation 5s infinite";
+}
+function normal3() {
+  document.getElementById("link3").style.WebkitAnimation = "color_change_animation 5s infinite";
+  document.getElementById("link3").style.animation = "color_change_animation 5s infinite";
+}
+function normal4() {
+  document.getElementById("link4").style.WebkitAnimation = "color_change_animation 5s infinite";
+  document.getElementById("link4").style.animation = "color_change_animation 5s infinite";
+}
+
+// Profile Blur
+function unblur() {
+  document.getElementById("profile").style.filter = "blur(0px)";
+  document.getElementById("profile").style.Webkitfilter = "blur(0px)";
+}
