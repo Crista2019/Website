bd8bc242a5fe84387de9145c1cc830a7070a4f17
Crista Falk <Crista2019@users.noreply.github.com>
2017-08-31 09:23:36 -0700

    Update photos.js

--- JAVASCRIPT/Website3.0/photos.js
+++ JAVASCRIPT/Website3.0/photos.js
@@ -1,146 +1,36 @@
-// Directory Link Animations
-function colorChange1() {
-  document.getElementById("link1").style.WebkitAnimation = "color_change_animation 1s infinite";
-  document.getElementById("link1").style.animation = "color_change_animation 1s infinite";
-}
-function colorChange2() {
-  document.getElementById("link2").style.WebkitAnimation = "color_change_animation 1s infinite";
-  document.getElementById("link2").style.animation = "color_change_animation 1s infinite";
-}
-function colorChange3() {
-  document.getElementById("link3").style.WebkitAnimation = "color_change_animation 1s infinite";
-  document.getElementById("link3").style.animation = "color_change_animation 1s infinite";
-}
-function colorChange4() {
-  document.getElementById("link4").style.WebkitAnimation = "color_change_animation 1s infinite";
-  document.getElementById("link4").style.animation = "color_change_animation 1s infinite";
-}
-function normal1() {
-  document.getElementById("link1").style.WebkitAnimation = "color_change_animation 5s infinite";
-  document.getElementById("link1").style.animation = "color_change_animation 5s infinite";
-}
-function normal2() {
-  document.getElementById("link2").style.WebkitAnimation = "color_change_animation 5s infinite";
-  document.getElementById("link2").style.animation = "color_change_animation 5s infinite";
-}
-function normal3() {
-  document.getElementById("link3").style.WebkitAnimation = "color_change_animation 5s infinite";
-  document.getElementById("link3").style.animation = "color_change_animation 5s infinite";
-}
-function normal4() {
-  document.getElementById("link4").style.WebkitAnimation = "color_change_animation 5s infinite";
-  document.getElementById("link4").style.animation = "color_change_animation 5s infinite";
+function openModal() {
+	document.getElementById('myModal').style.display = "block";
 }
 
-// Profile Blur
-function unblur() {
-  document.getElementById("profile").style.filter = "blur(0px)";
-  document.getElementById("profile").style.Webkitfilter = "blur(0px)";
+function closeModal() {
+	document.getElementById('myModal').style.display = "none";
 }
 
-// Resume Zoom
-var hasBeenClicked = false;
-function zoom() {
-  if (!hasBeenClicked) {
-    document.getElementById("resume").style.animation = "zoom 3s 1";
-    document.getElementById("resume").style.WebkitAnimation = "zoom 3s 1"; /* Chrome, Safari, Opera */
-    document.getElementById("resume").style.width = '80%';
-    hasBeenClicked === true;
-  }
-  // else {
-  //   document.getElementById("resume").style.animation = "zoom 3s 1";
-  //   document.getElementById("resume").style.WebkitAnimation = "zoom 3s 1"; /* Chrome, Safari, Opera */
-  //   document.getElementById("resume").style.width = "200px";
-  //   hasBeenClicked === false;
-  // }
-}
-// Adjust the color pallete for chem essay (I am too indecisive to assign one definitively, anyway)
-var randColor;
-function colorSchemeShift() {
-  function color() {
-    var hexadecimal = Math.floor(Math.random()*256).toString(16);
-    return ("0"+String(hexadecimal)).substr(-2)
-  }
-  randColor = "#"+color()+color()+color();
-  // console.log(randColor);
-  document.body.style.backgroundColor = randColor;
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+	showSlides(slideIndex += n);
 }
 
-var currentFontSize = "xx-small";
-
-function mediumFont() {
-  var x = document.querySelectorAll("p");
-  for (var i=0; i < x.length; i++) {
-    x[i].style.fontSize = "18px";
-  }
-  currentFontSize = "xx-medium";
-}
-S
-function largeFont() {
-  var x = document.querySelectorAll("p");
-  for (var i=0; i < x.length; i++) {
-    x[i].style.fontSize = "21px";
-  }
-  currentFontSize = "xx-large";
+function currentSlide(n) {
+	showSlides(slideIndex = n);
 }
 
-function smallFont() {
-  var x = document.querySelectorAll("p");
-  for (var i=0; i < x.length; i++) {
-    x[i].style.fontSize = "15px";
-  }
-  currentFontSize = "xx-small";
-}
-
-function hugeFont() {
-  var x = document.querySelectorAll("p");
-  for (var i=0; i < x.length; i++) {
-    x[i].style.fontSize = "25px";
-  }
-  currentFontSize = "xx-bigly";
-}
-
-function increaseFontSize() {
-  switch (currentFontSize) {
-    case "xx-small":
-      mediumFont();
-    break;
-    case "xx-medium":
-      largeFont();
-    break;
-    case "xx-large":
-      hugeFont();
-    break;
-    default:
-      hugeFont();
-    break;
-  }
-}
-
-function decreaseFontSize() {
-  switch (currentFontSize) {
-    case "xx-bigly":
-      largeFont();
-    break;
-    case "xx-large":
-      mediumFont();
-    break;
-    case "xx-medium":
-      smallFont();
-    break;
-    default:
-      smallFont();
-    break;
-  }
-}
-
-// Hide Obnoxious Directory Content
-function dir_hide() {
-  document.getElementById("down").style.display = "block";
-  document.getElementById("directory_text").style.display = "none";
-}
-
-function dir_show() {
-  document.getElementById("down").style.display = "none";
-  document.getElementById("directory_text").style.display = "block";
+function showSlides(n) {
+	var i;
+	var slides = document.getElementsByClassName("mySlides");
+	var dots = document.getElementsByClassName("view");
+	var captionText = document.getElementById("caption");
+	if (n > slides.length) {slideIndex = 1}
+	if (n < 1) {slideIndex = slides.length}
+	for (i = 0; i < slides.length; i++) {
+		slides[i].style.display = "none";
+	}
+	for (i = 0; i < dots.length; i++) {
+		dots[i].className = dots[i].className.replace(" active", "");
+	}
+	slides[slideIndex-1].style.display = "block";
+	dots[slideIndex-1].className += " active";
+	captionText.innerHTML = dots[slideIndex-1].alt;
 }
